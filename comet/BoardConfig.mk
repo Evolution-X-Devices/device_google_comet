e76dd3bc11fe78adbb4e188b9901332187ca6723
AnierinB <anierin@evolution-x.org>
2024-11-21 16:05:02 -0800

    comet: Initialize for Evolution X 11.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- comet/BoardConfig.mk
+++ comet/BoardConfig.mk
@@ -35,3 +35,4 @@ include device/google/comet/wifi/BoardConfig-wifi.mk
 DEVICE_PATH := device/google/comet
 VENDOR_PATH := vendor/google/comet
 include $(DEVICE_PATH)/$(TARGET_BOOTLOADER_BOARD_NAME)/BoardConfigLineage.mk
+include $(DEVICE_PATH)/$(TARGET_BOOTLOADER_BOARD_NAME)/BoardConfigEvolution.mk
